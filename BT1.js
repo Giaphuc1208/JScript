dce1ba524e5d992858df9852ba5a0462f4b0c063
giaphuccts <giaphuccts@gmail.com>
2024-03-07 15:44:00 +0700

    BT4NCC

--- BT1.js
+++ BT1.js
@@ -2,7 +2,7 @@
 let b = 5;
 let c= 20;
 let d= 15;
-const result = a + b * c - d / 2;
+const result = a + (b * c) - (d / 2);
 alert("Kết quả: " + result)*/
 
 
@@ -11,7 +11,7 @@ let b = 5
 let c = 20
 let d = 15
 
-let result = a+b *c - d /2
+let result = a+(b *c) - (d /2)
 
 // xuất kết quả
 console.log("Kết quả : " + result)*/
@@ -20,6 +20,6 @@ let a = 10;
 let b = 5;
 let c = 20;
 let d = 15;
-let result = a+b*c-d/2
+let result = a+(b*c)-(d/2)
 document.write(result)
 
